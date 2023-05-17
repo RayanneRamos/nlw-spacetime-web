ef228a8a4d7a784de1ffa303c62d3cc421a2a781
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-05-17 14:59:44 -0300

    criando o Hero e a sua estilização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { User } from 'lucide-react'
-// import nlwLogo from '../assets/nlw-space-logo.svg'
-// import Image from 'next/image'
+import nlwLogo from '../assets/nlw-space-logo.svg'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -21,6 +21,25 @@ export default function Home() {
             memórias!
           </p>
         </a>
+        {/* Hero */}
+        <div className="space-y-5 ">
+          <Image src={nlwLogo} alt="NLW Spacetime" />
+          <div className="max-w-[420px] space-y-4">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+          <a
+            href="#"
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+          >
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
       </div>
       {/* Right */}
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
